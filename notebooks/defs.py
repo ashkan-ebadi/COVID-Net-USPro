38e6e8197c71fb15e1fac2a6308b1546733bc5f3
Jessy Song <46058482+Jessyjias@users.noreply.github.com>
2022-04-26 13:31:32 -0400

    updated notebook files

--- notebooks/defs.py
+++ notebooks/defs.py
@@ -3,30 +3,28 @@ import numpy as np
 import cv2
 from scipy import ndimage
 
-# read images 
-def read_alphabets(class_directory_path, class_directory_name):
+def read_subdir_image(class_directory_path, class_directory_name):
     """
-    Reads all the characters/classes from a given alphabet_directory
+    Load image data from a  given a directory. 
+    Args:
+        class_directory_path: directory with images separated into class. Each class is a different folder.
+        class_directory_name: name of the classes. 
     """
     datax = []
     datay = []
     images = os.listdir(class_directory_path)
     for img in images:
         if not img.startswith('.') and "convex" in img: 
-        # if not img.startswith('.') : 
-            # print("reading image:" + img)
             image = cv2.resize(cv2.imread(class_directory_path + '/' + img), (224,224))
             # rotations of image 
             rotated_90 = ndimage.rotate(image, 90)
             rotated_180 = ndimage.rotate(image, 180)
             rotated_270 = ndimage.rotate(image, 270)
-            # datax.extend((image, rotated_90, rotated_180, rotated_270))
-            # datay.extend((
-            #     class_directory_name,
-            #     class_directory_name,
-            #     class_directory_name,
-            #     class_directory_name , 
-            # ))
-            datax.append(image)
-            datay.append(class_directory_name)
+            datax.extend((image, rotated_90, rotated_180, rotated_270))
+            datay.extend((
+                class_directory_name,
+                class_directory_name,
+                class_directory_name,
+                class_directory_name, 
+            ))
     return np.array(datax), np.array(datay)
--- notebooks/defs_LUS.py
+++ notebooks/defs_LUS.py
@@ -3,9 +3,13 @@ import numpy as np
 import cv2
 from scipy import ndimage
 
-def read_image_with_score(class_directory_path, class_directory_name, target_vid_names):
+def read_subdir_image(class_directory_path, class_directory_name, target_vid_names):
     """
-    Reads all the characters/classes from a given alphabet_directory
+    Load image data from a  given a directory. 
+    Args:
+        class_directory_path: directory with images separated into class. Each class is a different folder.
+        class_directory_name: name of the classes. 
+        target_vid_names: names of the videos with targetted LUS score. 
     """
     datax = []
     datay = []
@@ -13,19 +17,16 @@ def read_image_with_score(class_directory_path, class_directory_name, target_vid
     for img in images:
         contains_in_target_vid = len([string for string in target_vid_names if img.startswith(string)]) != 0
         if not img.startswith('.') and "convex" in img and contains_in_target_vid: 
-            # print("reading image:" + img)
             image = cv2.resize(cv2.imread(class_directory_path + '/' + img), (224,224))
             # rotations of image 
             rotated_90 = ndimage.rotate(image, 90)
             rotated_180 = ndimage.rotate(image, 180)
             rotated_270 = ndimage.rotate(image, 270)
-            # datax.extend((image, rotated_90, rotated_180, rotated_270))
-            # datay.extend((
-            #     class_directory_name,
-            #     class_directory_name,
-            #     class_directory_name,
-            #     class_directory_name , 
-            # ))
-            datax.append(image)
-            datay.append(class_directory_name)
+            datax.extend((image, rotated_90, rotated_180, rotated_270))
+            datay.extend((
+                class_directory_name,
+                class_directory_name,
+                class_directory_name,
+                class_directory_name , 
+            ))
     return np.array(datax), np.array(datay)
